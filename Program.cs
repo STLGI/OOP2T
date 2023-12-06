46c1d5603f3f69d519041290413bf66a3c8b421d
Stanislau Linnik <lstas102938@gmail.com>
2023-12-06 15:55:32 +0300

    Lesson 5

--- Program.cs
+++ Program.cs
@@ -7,12 +7,24 @@ namespace OOP2T
 	{  
 		static void Main()
 		{
-			Plane plane1 = new(10000, 200,1243.5, 653.1, 1000000.5, 400, 2012);
-			Car car1 = new(243.1, 639.5, 10000.3, 120, 1999);
-			Ship ship1 = new(600, "Port of Baltimore", 54873.5, 613.5, 10000000.5, 100, 2013);
-			Owner businessman = new(new List<Vehicle>{ plane1, car1 });
-			businessman.AddVehicle(ship1);
-			businessman.ShowVehicles();
+			try { 
+				Plane plane1 = new(10000, 200, 1243.5, 653.1, 1000000.5, 400, 2012);
+				Car car1 = new(243.1, 639.5, 10000.3, -120, 1999);
+				Ship ship1 = new(600, "Port of Baltimore", 54873.5, 613.5, 10000000.5, 100, 2013);
+				Owner businessman = new(new List<Vehicle> { plane1, car1 });
+				businessman.AddVehicle(ship1);
+				businessman.ShowVehicles();
+			}
+			catch (VehicleException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			catch
+			{
+				Console.WriteLine("Произошла непредвиденная ошибка");
+			}
+
+			
 		}
 	}
 }
--- Vehicle.cs
+++ Vehicle.cs
@@ -7,21 +7,40 @@ using System.Threading.Tasks;
 
 namespace OOP2T
 {
-	public abstract class Vehicle
+	class VehicleException : Exception
+	{
+		public VehicleException(string message) :base(message){ }
+	}
+	public interface IMovable
+	{
+		double X { get; set; }
+		double Y { get; set; }
+		double Price { get; set; }
+		int Speed { get; set; }
+		int YOI { get; set; }
+		void ShowInfo();
+	}
+	public abstract class Vehicle : IMovable
 	{
 		private double _x, _y, _price;
 		private int _speed, _yOI;
 
 		public double X { get; set; }
 		public double Y { get; set; }
-		public double Price { get; set; }
-		public int Speed { get; set; }
+		public double Price { get { return _price; } set { 
+				if (value < 0) throw new VehicleException("Цена не может быть отрицательной!");
+				else _price = value;  } }
+		public int Speed { get { return _speed; } set { 
+				if (value < 0) throw new VehicleException("Скорость не может быть отрицательной!");
+				else _speed = value;  } }
 		public int YOI { get; set; }
 
-		public Vehicle(double x, double y, double price) { 
+		public Vehicle(double x, double y, double price, int speed, int yoI) { 
 			X = x;
 			Y = y;
 			Price = price;
+			Speed = speed;
+			YOI = yoI;
 		}
 
 		public virtual void ShowInfo()
@@ -36,15 +55,17 @@ namespace OOP2T
 		private float _height;
 		private int _passengers;
 
-		public float Height { get; set; }
-		public int Passengers { get; set; }
+		public float Height { get { return _height; } set { 
+				if (value < 0) throw new VehicleException("Высота не может быть отрицательной!");
+				else _height = value;  } }
+		public int Passengers { get { return _passengers; } set { 
+				if (value < 0) throw new VehicleException("Число пассажиров не может быть отрицательным!");
+				else _passengers = value;  } }
 		
-		public Plane(float height, int passengers, double x, double y, double price, int speed, int yOI) : base(x, y, price)
+		public Plane(float height, int passengers, double x, double y, double price, int speed, int yOI) : base(x, y, price, speed, yOI)
 		{
 			Height = height;
 			Passengers = passengers;
-			Speed = speed;
-			YOI = yOI;
 		}
 		public override void ShowInfo()
 		{
@@ -54,10 +75,9 @@ namespace OOP2T
 	}
 	public class Car : Vehicle
 	{
-		public Car(double x, double y, double price, int speed, int yoI):base(x, y, price)
+		public Car(double x, double y, double price, int speed, int yOI):base(x, y, price, speed, yOI)
 		{
-			Speed = speed;
-			YOI = yoI;
+			
 		}
 		public override void ShowInfo()
 		{
@@ -70,14 +90,14 @@ namespace OOP2T
 		private int _passengers; 
 		private string? _port;
 
-		public int Passengers { get; set; }
+		public int Passengers { get { return _passengers; } set { 
+				if (value < 0) throw new VehicleException("Число пассажиров не может быть отрицательным!");
+				else _passengers = value;  } }
 		public string? Port { get; set; }
-		public Ship(int passengers, string? port, double x, double y, double price, int speed, int yOI) : base(x, y, price)
+		public Ship(int passengers, string? port, double x, double y, double price, int speed, int yOI) : base(x, y, price, speed, yOI)
 		{
 			Passengers = passengers;
 			Port = port;
-			Speed = speed;
-			YOI = yOI;
 		}
 		public override void ShowInfo()
 		{
